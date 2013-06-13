00205cfad38f1dd7319dcd61fe88f23f583c433e
sk2koji <sk2koji@server.fake>
2013-06-13 23:59:12 +0900

    create project

---
+++ src/main/java/solr/custom/sort/App.java
@@ -0,0 +1,11 @@
+package solr.custom.sort;
+
+/**
+ * Hello world!
+ * 
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/solr/custom/sort/AppTest.java
@@ -0,0 +1,14 @@
+package solr.custom.sort;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+public class AppTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
